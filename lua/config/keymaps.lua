a364929c3da4cf30d1171830bab4008614f936d9
victorwk93812 <victorwk93812@gmail.com>
2024-12-27 22:39:13 +0800

    telescope old files, vimtex okular

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -131,6 +131,7 @@ function M.bind_telescope_keys()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope old files' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 end
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,7 +5,13 @@ return {
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
 		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_view_method = "zathura"
+        -- Using zathura 
+		-- vim.g.vimtex_view_method = "zathura" -- zathura
+
+        -- Using Okular
+        vim.g.vimtex_view_general_viewer = 'okular'
+        vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+
 		vim.g.vimtex_quickfix_open_on_warning = 0
 		-- Add option --shell-escape to vimtex
 		vim.g.vimtex_compiler_latexmk = {

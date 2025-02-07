0883a83254970969c16ec4befceef50e952fcd49
victorwk93812 <victorwk93812@gmail.com>
2025-02-08 02:18:46 +0800

    Removed ensure install clangd LS

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,7 +33,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-				"clangd", 
+				-- "clangd", 
             },
             handlers = {
                 function(server_name) -- default handler (optional)

fc18b444c289ffe7d5824caf1555f1a4b2fa4567
victorwk93812 <victorwk93812@gmail.com>
2025-02-22 21:17:57 +0800

    Removed lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,8 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "pyright", 
+                -- "texlab",
 				-- "clangd", 
             },
             handlers = {
